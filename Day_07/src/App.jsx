a43642741119649f55ddbbee7db0b042bdae9566
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-08-09 16:34:37 +0000

    4

--- Day_07/src/App.jsx
+++ Day_07/src/App.jsx
@@ -1,32 +1,39 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Navbar from './components/Navbar'
+import './App.css'
 
 function App() {
-  const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState([])
 
   const fetchData = async () => {
-    let a = await fetch("https://jsonplaceholder.typicode.com/posts");
-    let data = await a.json();
-    setCards(data);
-    console.log(data);
-  };
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data = await a.json()
+    setCards(data)
+    console.log(data)
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
+
 
   return (
-    <div className='container'>
-      <div className='cards'>
-        {cards.map(card => (
-          <div key={card.id} className="card">
-            <h3>{card.title}</h3>
-            <p>{card.body}</p>
-          </div>
-        ))}
+    <>
+    <Navbar/> 
+      <div className="container">
+       {cards.map((card)=>{
+        return <div key={card.id} className="card">
+          <h1>{card.title}</h1>
+          <p>{card.body}</p>
+          <span>By: UserId: {card.userId} </span>
+        </div>
+
+       })}
+        
       </div>
-    </div>
-  );
+
+    </>
+  )
 }
 
-export default App;
+export default App
