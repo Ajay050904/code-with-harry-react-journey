6650e4f4e4c18dba6d8172b69e5d9f1a16957796
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-08-11 16:24:01 +0000

    3

--- Day_08/src/App.jsx
+++ Day_08/src/App.jsx
@@ -1,15 +1,55 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  // const [name, setName] = useState("MAHAM");
+  const [form, setForm] = useState({email: "", phone: ""});
+
   const handleClick = () => {
     alert("Hey I am clicked");
   };
 
+  // const handleMouseOver = () => {
+  //   alert("Hey I am a mouse over");
+  // };
+
+  // const handleChange = (e) => {
+  //   setName(e.target.value);
+  // };
+
+  const handleChange = (e) => {
+    setForm({...form,[e.target.name]:e.target.value});
+    console.log(form);
+    
+  };
+
+
+
   return (
     <>
       <div className="button">
-        <button onClick={handleClick()}>Click me</button>
+        <button onClick={handleClick}>Click me</button>
       </div>
+
+      {/* <div className="red" onMouseOver={handleMouseOver}>
+        I am a red div
+      </div> */}
+
+      {/* <input type="text" value={name} onChange={handleChange} /> */}
+
+      <input
+        type="text"
+        name="email"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="phone"
+        value={form.phone}
+        onChange={handleChange}
+      />
     </>
   );
 }
