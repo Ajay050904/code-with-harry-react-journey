425b8f56e7ccf432addf9928b9a345cc94822e50
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-08-17 17:39:09 +0000

    4

--- Day_10/src/components/component1.jsx
+++ Day_10/src/components/component1.jsx
@@ -1,14 +1,9 @@
-import React, { useContext } from 'react'
-import { counterContext } from '../context/context'
-
+import React, { useContext } from "react";
+import { counterContext } from "../context/context";
 
 const Component1 = () => {
-  const value = useContext(counterContext)
-  return (
-    <div>
-    {value.count}
-    </div>
-  )
-}
+  const value = useContext(counterContext);
+  return <div>{value.count}</div>;
+};
 
-export default Component1
+export default Component1;
